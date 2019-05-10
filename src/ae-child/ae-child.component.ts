6587e3ce51501e5dba73404918252d0406c326f8
j0wey <webmaster@joel-wochele.de>
2019-05-10 12:53:06 +0200

    add basic routing example

---
+++ src/ae-child/ae-child.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ae-child',
+  templateUrl: './ae-child.component.html',
+  styleUrls: ['./ae-child.component.css']
+})
+export class AeChildComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,18 +2,31 @@ import { NgModule, DoBootstrap, Input, Output, Injector } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { Component } from '@angular/core';
 import { createCustomElement } from '@angular/elements';
+import { RouterModule } from '@angular/router';
+import { AeChildComponent } from './ae-child/ae-child.component';
 
 
 @Component({
   selector: 'my-app', /* never used */
-  template: `Start editing to see some magic happen :)`
+  template: `
+  <ul>
+    <li><a routerLink="/ae/child">Route to child</a></li>
+  </ul>
+  <router-outlet></router-outlet>
+  `
 })
 export class AppComponent {
 }
 
 @NgModule({
-  imports: [BrowserModule],
-  declarations: [AppComponent],
+  imports: [BrowserModule,
+    RouterModule.forRoot(
+      [
+        { path: 'ae/child', component: AeChildComponent }
+      ]
+    )
+  ],
+  declarations: [AppComponent, AeChildComponent],
   entryComponents: [
     AppComponent
   ],
